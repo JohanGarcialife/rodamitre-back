608145a2de7fdb31ed4375ff6c5350fd83d8521e
Johan <johan.garcia165@gmail.com>
2024-05-03 15:20:03 -0400

    mi primer commit

---
+++ src/database/connections.js
@@ -0,0 +1,23 @@
+import sql from 'mssql'
+
+const dbSettings = {
+    user: "SA", ////este usuario es por defecto, si no te agarra a ti seguro es en minusculas
+    password:"4L3x_R0m4n", /// clave del servidor 
+    server:"10.0.0.237",
+    database: "DB_RODAMITRE_WEB_ATIKA",////nombre de la base de datos
+    options: {
+        encrypt:false,
+        trustServerCertificate: true
+    }
+}
+
+
+export const getConnection = async () => {
+    try { 
+const pool = await sql.connect(dbSettings)
+    return pool;
+    } catch (error){
+        console.log(error)
+    }   
+}
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import app from "./app.js"
+import { getConnection } from "./database/connections.js"
+
+//getConnection()
+app.listen(5000)
+
+console.log("servidor iniciado") 
